fb66e12ec1eb66db461823d22ca0670187c403e9
GB010101 <88103093+GB010101@users.noreply.github.com>
2023-04-06 19:44:33 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,8 @@
+function executaSoma()
+{
+    let x = document.getElementById("x");
+    let vx = x.value;
+    let resultado = document.getElementById("resultado");
+    alert("Obrigado pela resposta");
+
+}
